ce617004cdafecc1381624dcfcd0371f18c484af
Stefania9797 <stefaniamastrel@gmail.com>
2021-06-21 14:20:49 +0200

    stampa paragrafo a schermo

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+$text= "Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet consectetur veniam Lorem ipsum dolor Lorem adipisicing elit. Dolorum, Lorem illo! Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem, eligendi. Lorem Molestiae?"
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Intro</title>
+</head>
+<body>
+<p><?php echo $text; ?></p>
+<p>Il paragrafo è lungo: <?php echo strlen($text); ?> caratteri</p>
+</body>
+</html>
